d237c28f060809234eb7f3dae803a874295bbfe8
Dmiriy <152209418+DmitriyKash@users.noreply.github.com>
2024-05-16 16:04:18 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 import HomePage from './pages/HomePage';
 
 const App = () => {
+  useEffect(() => {
+    console.log('Initializing Telegram Web App...');
+    if (window.Telegram.WebApp) {
+      window.Telegram.WebApp.ready();
+    } else {
+      console.error('Telegram Web App not found');
+    }
+  }, []);
+
   return (
     <TonConnectUIProvider manifestUrl="https://magnificent-bonbon-998142.netlify.app/manifest.json">
       <Router>
